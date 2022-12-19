1f57400db60854862c55c6684686d9ce19deaa5b
sojeonglee <sojeonglee0125@gmail.com>
2022-12-19 17:20:06 +0900

    feat : 태그 붙이기 테스트

---
+++ src/test/java/github_test/todo/service/TodoServiceTest.java
@@ -0,0 +1,29 @@
+package github_test.todo.service;
+
+import github_test.todo.controller.TodoController;
+import github_test.todo.domain.Board;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TodoServiceTest {
+
+    @Autowired
+    private TodoService todoService;
+    //ddd
+
+    @Test
+    void crollingTest() throws Exception {
+        List<Board> crolling = todoService.crolling();
+
+        for (Board board : crolling) {
+            System.out.println("board = " + board);
+        }
+    }
+
+}
